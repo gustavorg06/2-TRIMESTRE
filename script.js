ccfb9cd2c17840bd4b3efe5611453462b35fe9bf
gustavorg06 <161603093+gustavorg06@users.noreply.github.com>
2024-05-28 08:49:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const caixaPrincipal=document.querySelector(".caixa-pricipal");
+const caixaPerguntas=document.querySelector("caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
 
+const perguntas = [
+  {
+    enunciado:"Qual o time com mais Brasileirões?",
+    alternativas:[
+      "Palmeiras",
+      "flamengo",
+      ]
+  },
+[
+  {
+    enunciado:"Qual ano o palmeiras conquistou sua ultima libertadores",
+    alternativas:[
+      "2021",
+      "2020",
+      ]
+  },
+  [
+  {
+    enunciado:"quem foi que marcou o gol da vitoria contra o snatos na final da libertadores de 2020",
+    alternativas:[
+      "rony",
+      "breno lopes",
+      ]
+  },
+    [
+  {
+    enunciado:"quantos titulos mundias o Brasil tem?",
+    alternativas:[
+      "6",
+      "5",
+      ]
+  },
+      [
+  {
+    enunciado:"Pergunta 1",
+    alternativas:[
+      "alternativa 1",
+      "alternativa 2",
+      ]
+  },
+ ];       
